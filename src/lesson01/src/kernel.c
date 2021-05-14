87657965781bba4a7468b62d1c55842518867e74
nemesis5324 <hoangson88h@gmail.com>
2021-05-14 22:42:30 +0700

    run all 4 cores

--- src/lesson01/src/kernel.c
+++ src/lesson01/src/kernel.c
@@ -1,11 +1,36 @@
 #include "mini_uart.h"
 
-void kernel_main(void)
+void kernel_main(int core)
 {
 	uart_init();
-	uart_send_string("Hello, world!\r\n");
+	if(core==0){
+		uart_send_string("Hello from CPU number 0\r\n");
 
-	while (1) {
-		uart_send(uart_recv());
+		while (1) {
+			uart_send(uart_recv());
+		}
 	}
+	if(core==1){
+                uart_send_string("Hello from CPU number 1\r\n");
+
+                while (1) {
+                        uart_send(uart_recv());
+                }
+        }
+	if(core==2){
+                uart_send_string("Hello from CPU number 2\r\n");
+
+                while (1) {
+                        uart_send(uart_recv());
+                }
+        }
+	if(core==3){
+                uart_send_string("Hello from CPU number 3\r\n");
+
+                while (1) {
+                        uart_send(uart_recv());
+                }
+        }
+
+
 }
