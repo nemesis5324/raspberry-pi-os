e9e63948e2c5cc3b2cf2a40509dd318d9a7da54e
nemesis5324 <hoangson88h@gmail.com>
2021-05-15 08:58:00 +0700

    added delay

--- src/lesson01/src/kernel.c
+++ src/lesson01/src/kernel.c
@@ -2,29 +2,20 @@
 
 void kernel_main(int core)
 {
-	uart_init();
 	if(core==0){
+		uart_init();
 		uart_send_string("Hello from CPU number 0\r\n");
-
-		while (1) {
-			uart_send(uart_recv());
-		}
 	}
 	if(core==1){
+		delay(125000000);
                 uart_send_string("Hello from CPU number 1\r\n");
-
-                while (1) {
-                        uart_send(uart_recv());
-                }
         }
 	if(core==2){
+		delay(250000000);
                 uart_send_string("Hello from CPU number 2\r\n");
-
-                while (1) {
-                        uart_send(uart_recv());
-                }
         }
 	if(core==3){
+		delay(375000000);
                 uart_send_string("Hello from CPU number 3\r\n");
 
                 while (1) {
