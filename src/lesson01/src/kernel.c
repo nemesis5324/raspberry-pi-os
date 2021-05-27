b6c960b1cf372c384c4f1e10bd6240b0f8d86ea3
nemesis5324 <hoangson88h@gmail.com>
2021-05-27 14:18:59 +0700

    all 4 cores work, make delicated stack for every processor

--- src/lesson01/src/kernel.c
+++ src/lesson01/src/kernel.c
@@ -1,27 +1,31 @@
+#include "utils.h"
+#include "mm.h"
 #include "mini_uart.h"
 
-void kernel_main(int core)
+static unsigned int processor = 0;	// global variable to sinc the processors
+
+void kernel_main(unsigned int  proc_id)		// get processor id as 'char' from 'x0'
 {
-	if(core==0){
+
+	while (processor != proc_id) {}	// wait to execute
+
+	if (proc_id == 0) {				// only the master (processor id = 0) initialize uart
 		uart_init();
-		uart_send_string("Hello from CPU number 0\r\n");
 	}
-	if(core==1){
-		delay(125000000);
-                uart_send_string("Hello from CPU number 1\r\n");
-        }
-	if(core==2){
-		delay(250000000);
-                uart_send_string("Hello from CPU number 2\r\n");
-        }
-	if(core==3){
-		delay(375000000);
-                uart_send_string("Hello from CPU number 3\r\n");
 
-                while (1) {
-                        uart_send(uart_recv());
-                }
-        }
+    if(processor==0)uart_send_string("This is core number 0 \r\n");
+    if(processor==1)uart_send_string("This is core number 1 \r\n");
+    if(processor==2)uart_send_string("This is core number 2 \r\n");
+    if(processor==3)uart_send_string("This is core number 3 \r\n");
+	processor++;					// increment 'processor' to enable the next core to execute
 
+	if (proc_id == 3){
+		while (1) {
+			uart_send(uart_recv());	// only the master echos the typed character through uart
+		}
+
+	} else {
+		while (1) {}				// all the other cores in loop here
+	}
 
 }
